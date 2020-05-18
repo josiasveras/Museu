ce49800d0702aed03a12717f1a0f86a912913fcf
Jorzias Veras <josias_veras@hotmail.com>
2020-05-17 23:45:01 -0300

    Add general updates

--- sql/BD_Museu.sql
+++ sql/BD_Museu.sql
@@ -19,11 +19,23 @@ CREATE TABLE `obra`
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `usuario_obra_id` int,
   `nome_obra` varchar(50) NOT NULL,
-  `foto_obra` blob NULL,
   `descricao` varchar(600) NOT NULL,
   `data` datetime default current_timestamp
 );
 
 INSERT INTO `obra`(`usuario_obra_id`, `nome_obra`, `descricao`) VALUES (1,'meme', 'Meme mais recente');
 
+CREATE TABLE `imagem` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `obra_imagem_id` int,
+  `usuario_imagem_id` int,
+  `arquivo` varchar(500) NOT NULL,
+  `caminho` varchar(500) NOT NULL,
+  `tamanho` float NOT NULL,
+  `fecha` bigint(20) NOT NULL
+);
+
 ALTER TABLE `obra` ADD FOREIGN KEY (`usuario_obra_id`) REFERENCES `usuario` (`id`);
+ALTER TABLE `imagem` ADD FOREIGN KEY (`obra_imagem_id`) REFERENCES `obra` (`id`);
+ALTER TABLE `imagem` ADD FOREIGN KEY (`usuario_imagem_id`) REFERENCES `usuarios` (`id`);
+
