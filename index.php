39bdf95eab4ed866153d924c52d337c6af5c3d38
Jorzias Veras <josias_veras@hotmail.com>
2020-05-17 23:23:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Museu SENAC</title>
-        <meta charset="UTF-8">
 
-        <!-- Normalize CSS -->
-        <link rel="stylesheet" type="text/css" href="css/normalize.css">
-        
-        <!-- Estilo customizado -->
-        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+<?php require "php/cabecalho.php" ?>
 
-        <!-- html5shiv para fazer com que os navegadores que não reconheçam as novas tags HTML5, passem a reconhecê-las -->
-        <!--[if lt IE 9]>
-	        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
-        <![endif]-->
-
-    </head>
     <body>
 
         <!-- Início container -->
@@ -160,17 +145,7 @@
                 </aside>
                 <!-- Fim aside -->
 
-                <!-- Limpando o fluxo flutuante -->
-                <footer>
-                    <p>
-                        <a href="#">Home</a>
-                        <a href="#">Exposições</a>
-                        <a href="#">Contatos</a>
-                    </p>
-                    <p>
-                        2020 <a href="#">Museu SENAC</a> - Todos os direitos reservados.
-                    </p>
-                </footer>
+                <?php require "php/rodape.php" ?>
 
             </div>
             <!-- Fim principal -->
