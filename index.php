9f7728609dc23117e0872e8430dc3712e80f0272
Jorzias Veras <josias_veras@hotmail.com>
2020-05-26 18:53:05 -0300

    General updates

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 
-<?php require "php/cabecalho.php" ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Museu SENAC</title>
+        <meta charset="UTF-8">
+
+        <!-- Normalize CSS -->
+        <link rel="stylesheet" type="text/css" href="css/normalize.css">
+        
+        <!-- Estilo customizado -->
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+        <!-- html5shiv para fazer com que os navegadores que não reconheçam as novas tags HTML5, passem a reconhecê-las -->
+        <!--[if lt IE 9]>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
+        <![endif]-->
+
+    </head>
 
     <body>
 
@@ -145,7 +162,16 @@
                 </aside>
                 <!-- Fim aside -->
 
-                <?php require "php/rodape.php" ?>
+                <footer>
+                    <p>
+                        <a href="#">Home</a>
+                        <a href="#">Exposições</a>
+                        <a href="#">Contatos</a>
+                    </p>
+                    <p>
+                        2020 <a href="#">Museu SENAC</a> - Todos os direitos reservados.
+                    </p>
+                </footer>
 
             </div>
             <!-- Fim principal -->
--- sql/BD_Museu.sql
+++ sql/BD_Museu.sql
@@ -1,41 +1,33 @@
 CREATE DATABASE Museu;
 USE Museu;
 
-CREATE TABLE `usuario`
+CREATE TABLE `usuarios`
 (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) NOT NULL,
+  `id_usuario` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nome_usuario` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `senha` varchar(32) NOT NULL,
   `dt_nasc` date NOT NULL,
-  `genero` varchar(2) NOT NULL,
-  `estado` varchar(2) NOT NULL
+  `genero` varchar(2) NOT NULL
 );
 
-INSERT INTO `usuario`(`nome`, `email`, `senha`, `dt_nasc`, `genero`, `estado`) VALUES ('Jorzias Veras','jorzias_veras@colabora.com.br',(md5(123)),'1991-01-28','M','SP');
+INSERT INTO `usuarios`(`nome_usuario`, `email`, `senha`, `dt_nasc`, `genero`) VALUES ('Jorzias Veras','jorzias_veras@museu.com.br',(md5(123)),'1991-01-28','M');
 
-CREATE TABLE `obra`
+CREATE TABLE `obras`
 (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `usuario_obra_id` int,
+  `id_obra` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `fk_id_usuario` int,
   `nome_obra` varchar(50) NOT NULL,
   `descricao` varchar(600) NOT NULL,
-  `data` datetime default current_timestamp
+  `data` datetime default current_timestamp,
+    FOREIGN KEY(fk_id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE
 );
 
-INSERT INTO `obra`(`usuario_obra_id`, `nome_obra`, `descricao`) VALUES (1,'meme', 'Meme mais recente');
+INSERT INTO `obras`(`fk_id_usuario`, `nome_obra`, `descricao`) VALUES (1,'meme', 'Meme mais recente');
 
-CREATE TABLE `imagem` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `obra_imagem_id` int,
-  `usuario_imagem_id` int,
-  `arquivo` varchar(500) NOT NULL,
-  `caminho` varchar(500) NOT NULL,
-  `tamanho` float NOT NULL,
-  `data` datetime default current_timestamp
+CREATE TABLE imagens(
+  id_imagem int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  nome_imagem varchar(40),
+  fk_id_obra int,
+    FOREIGN KEY(fk_id_obra) REFERENCES obras(id_obra) ON DELETE CASCADE
 );
-
-ALTER TABLE `obra` ADD FOREIGN KEY (`usuario_obra_id`) REFERENCES `usuario` (`id`);
-ALTER TABLE `imagem` ADD FOREIGN KEY (`obra_imagem_id`) REFERENCES `obra` (`id`);
-ALTER TABLE `imagem` ADD FOREIGN KEY (`usuario_imagem_id`) REFERENCES `usuarios` (`id`);
-
