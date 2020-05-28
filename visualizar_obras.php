9e07df7390d27a28872cf4358e39290d03549ebf
Jorzias Veras <josias_veras@hotmail.com>
2020-05-28 00:47:26 -0300

    Create visualizar_obras.php

---
+++ visualizar_obras.php
@@ -0,0 +1,124 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Museu SENAC</title>
+        <?php require "./head.php"; ?>
+
+    </head>
+
+    <body>
+
+        <!-- Início container -->
+        <div id="container">
+
+            <!-- Início header -->
+            <header>
+                <div id="logo">
+                    <h1><a href="">MuSenac</a></h1>
+                </div>
+
+                <!-- Início nav -->
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Exposições</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </nav>
+                <!-- Fim nav -->
+
+            </header>
+            <!-- Fim header -->
+
+            <!-- Início principal -->
+            <div id="principal">
+
+
+                <br><br><br><br><br><br>
+                <div class="container">
+
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="thumbnail">
+                          <a href="#">
+                            <img src="./img/van-gogh.jpg" alt="Lights" style="width:100%">
+                            <div class="caption">
+                              <p>Van Gogh</p>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="thumbnail">
+                          <a href="#">
+                            <img src="./img/van-gogh.jpg" alt="Nature" style="width:100%">
+                            <div class="caption">
+                              <p>Van Gogh</p>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="thumbnail">
+                          <a href="#">
+                            <img src="./img/van-gogh.jpg" alt="Fjords" style="width:100%">
+                            <div class="caption">
+                              <p>Van Gogh</p>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="thumbnail">
+                          <a href="#">
+                            <img src="./img/van-gogh.jpg" alt="Fjords" style="width:100%">
+                            <div class="caption">
+                              <p>Van Gogh</p>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="thumbnail">
+                          <a href="#">
+                            <img src="./img/van-gogh.jpg" alt="Fjords" style="width:100%">
+                            <div class="caption">
+                              <p>Van Gogh</p>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="thumbnail">
+                          <a href="#">
+                            <img src="./img/van-gogh.jpg" alt="Fjords" style="width:100%">
+                            <div class="caption">
+                              <p>Van Gogh</p>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+
+                </div>
+
+                <footer>
+                    <p>
+                        <a href="#">Home</a>
+                        <a href="#">Exposições</a>
+                        <a href="#">Contatos</a>
+                    </p>
+                    <p>
+                        2020 <a href="#">Museu SENAC</a> - Todos os direitos reservados.
+                    </p>
+                </footer>
+
+            </div>
+            <!-- Fim principal -->
+
+        </div>
+        <!-- Fim container -->
+        
+    </body>
+</html>
