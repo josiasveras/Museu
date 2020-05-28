f83c5167560cbcc663ed879c8003d9f0d20fdc61
Jorzias Veras <josias_veras@hotmail.com>
2020-05-28 00:47:36 -0300

    Create visualizar_obra_perfil.php

---
+++ visualizar_obra_perfil.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Museu SENAC</title>
+        <?php require "./head.php"; ?>
+
+    </head>
+
+    <body>
+
+        <!-- Início container -->
+        <div id="container">
+
+            <!-- Início header -->
+            <header>
+                <div id="logo">
+                    <h1><a href="">MuSenac</a></h1>
+                </div>
+
+                <!-- Início nav -->
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Exposições</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </nav>
+                <!-- Fim nav -->
+
+            </header>
+            <!-- Fim header -->
+
+            <!-- Início principal -->
+            <div id="principal">
+                
+                 <br><br><br><br><br><br>
+                <div class="card">
+                    <img class="card-img" src="./img/van-gogh.jpg" alt="Card image">
+                </div>
+                <div class="card p-3 text-center">
+                    <blockquote class="blockquote mb-0">
+                        <h1>Van Gogh X</h1> 
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                      <footer class="blockquote-footer">
+                        <small class="text-muted">
+                          Por <cite title="Source Title">Jorzias Veras</cite>
+                        </small>
+                      </footer>
+                    </blockquote>
+                </div>
+                <br><br><br><br><br><br>
+
+                <footer>
+                    <p>
+                        <a href="#">Home</a>
+                        <a href="#">Exposições</a>
+                        <a href="#">Contatos</a>
+                    </p>
+                    <p>
+                        2020 <a href="#">Museu SENAC</a> - Todos os direitos reservados.
+                    </p>
+                </footer>
+
+            </div>
+            <!-- Fim principal -->
+
+        </div>
+        <!-- Fim container -->
+        
+    </body>
+</html>
