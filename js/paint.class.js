edff2c5e2470ce357a317deff528b650d172861a
Jorzias Veras <josias_veras@hotmail.com>
2020-05-30 18:25:36 -0300

    Create paint.class.js
    Criando a classe com a lógica de desenhar as formas com o Mouse utilizando o Canvas

---
+++ js/paint.class.js
@@ -0,0 +1,63 @@
+import Point from './point.model.js';
+import {TOOL_LINE, TOOL_RECTANGLE, TOOL_CIRCLE, TOOL_TRIANGLE, TOOL_PAINT_BUCKET, TOOL_PENCIL, TOOL_BRUSH, TOOL_ERASER} from './tool.js';
+import { getMouseCoordsOnCanvas } from  './utility.js';
+
+export default class Paint{
+
+	constructor(canvasId){
+
+		this.canvas = document.getElementById(canvasId);
+		this.context = canvas.getContext("2d");
+
+	}
+
+	set activeTool(tool){
+		this.tool = tool;
+	}
+
+	init(){
+		this.canvas.onmousedown = e => this.onMouseDown(e);
+	}
+
+	onMouseDown(e){
+
+		this.savedData = this.context.getImageData(0, 0, this.canvas.clientWidth, this.canvas.height);
+
+		this.canvas.onmousemove = e => this.onMouseMove(e);
+		document.onmouseup = e => this.onMouseUp(e);
+
+		this.starPos = getMouseCoordsOnCanvas(e, this.canvas);
+
+		console.log(this.starPos);
+
+	}
+
+	onMouseMove(e){
+		this.currentPos = getMouseCoordsOnCanvas(e, this.canvas);
+		
+		switch(this.tool){
+			case TOOL_LINE:
+				this.drawShape();
+				break;
+			default:
+				break;
+		}
+	}
+
+	onMouseUp(e){
+		this.canvas.onmousemove = null;
+		document.onmouseup = null;
+	}
+
+	drawShape(){
+
+		this.context.putImageData(this.savedData, 0, 0);
+
+		this.context.beginPath();
+		this.context.moveTo(this.starPos.x, this.starPos.y);
+		this.context.lineTo(this.currentPos.x, this.currentPos.y);
+		this.context.stroke();	
+
+	}
+
+}
