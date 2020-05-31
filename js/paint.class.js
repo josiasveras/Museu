c60350e542768d4a5417759eef7dbac094373879
Jorzias Veras <josias_veras@hotmail.com>
2020-05-30 22:04:47 -0300

    Update paint.class.js
    Lógica para desenhar o triângulo.

--- js/paint.class.js
+++ js/paint.class.js
@@ -41,6 +41,7 @@ export default class Paint{
 			case TOOL_LINE:
 			case TOOL_RECTANGLE:
 			case TOOL_CIRCLE:
+			case TOOL_TRIANGLE:
 				this.drawShape();
 				break;
 			default:
@@ -53,6 +54,7 @@ export default class Paint{
 		document.onmouseup = null;
 	}
 
+	//Função com a lógica para desenhar as formas geométricas
 	drawShape(){
 
 		this.context.putImageData(this.savedData, 0, 0);
@@ -60,13 +62,26 @@ export default class Paint{
 		this.context.beginPath();
 
 		if (this.tool == TOOL_LINE){
+
 			this.context.moveTo(this.starPos.x, this.starPos.y);
 			this.context.lineTo(this.currentPos.x, this.currentPos.y);
+
 		}else if(this.tool == TOOL_RECTANGLE){
+
 			this.context.rect(this.starPos.x, this.starPos.y, this.currentPos.x - this.starPos.x, this.currentPos.y - this.starPos.y);
+
 		}else if(this.tool == TOOL_CIRCLE){
+
 			let distance = findDistance(this.starPos, this.currentPos);
 			this.context.arc(this.starPos.x, this.starPos.y, distance, 0, 2 * Math.PI, false);
+
+		}else if(this.tool == TOOL_TRIANGLE){
+
+			this.context.moveTo(this.starPos.x + (this.currentPos.x - this.starPos.x) / 2, this.starPos.y);
+			this.context.lineTo(this.starPos.x, this.currentPos.y);
+			this.context.lineTo(this.currentPos.x, this.currentPos.y);
+			this.context.closePath();
+
 		}
 
 		this.context.stroke();	
