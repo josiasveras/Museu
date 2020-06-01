fd5f4f162c4ffbc8122d58af8fdc7050e267fd48
Jorzias Veras <josias_veras@hotmail.com>
2020-06-01 19:04:38 -0300

    Update cadastro_usuario.php

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -53,9 +53,9 @@
                 <!-- Início nav -->
                 <nav>
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Exposições</a></li>
-                        <li><a href="#">Contato</a></li>
+                        <li><a href="./index.php">Home</a></li>
+                        <li><a href="./visualizar_obras.php">Exposições</a></li>
+                        <li><a href="./criar_obra.php" target="_blank">Criar obra</a></li>
                     </ul>
                 </nav>
                 <!-- Fim nav -->
