dbd9a1d2bd26ba4af0107c8f20c94d24aeddf8cb
Jorzias Veras <josias_veras@hotmail.com>
2020-06-01 19:04:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -32,10 +32,11 @@
                 <!-- Início nav -->
                 <nav>
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Exposições</a></li>
-                        <li><a href="#">Contato</a></li>
+                        <li><a href="./index.php">Home</a></li>
+                        <li><a href="./visualizar_obras.php">Exposições</a></li>
+                        <li><a href="./criar_obra.php" target="_blank">Criar obra</a></li>
                     </ul>
+
                 </nav>
                 <!-- Fim nav -->
 
