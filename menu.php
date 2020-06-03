3e49eda01285ccc2e4cc8bef60f338fd232e044a
Jorzias Veras <josias_veras@hotmail.com>
2020-06-03 19:08:22 -0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Museu SENAC</title>
+        <?php require "./head.php"; ?>
+
+    </head>
+
+    <body>
+
+        <!-- Início container -->
+        <div id="container">
+
+
+    <!-- Início header -->
+    <header>
+        <div id="logo">
+            <h1><a href="">MuSenac</a></h1>
+        </div>
+
+        <!-- Início nav -->
+        <nav>
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="./visualizar_obras.php">Exposições</a></li>
+                <li><a href="./criar_obra.php" target="_blank">Criar obra</a></li>
+            </ul>
+
+        </nav>
+        <!-- Fim nav -->
+
+    </header>
+    <!-- Fim header -->
