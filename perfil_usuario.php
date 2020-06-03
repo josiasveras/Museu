e6eca448fd9a29334ca7f62a22873fde6f0b84df
Jorzias Veras <josias_veras@hotmail.com>
2020-06-03 14:36:27 -0300

    Update perfil_usuario.php
    Usando sessão para redirecionar a página.

--- perfil_usuario.php
+++ perfil_usuario.php
@@ -1,7 +1,13 @@
-<?php 
-    
-   require "./class-php/user.class.php";
-   $user = new User("museu", "localhost", "root", "");
+<?php
+
+    session_start();
+    if(!isset($_SESSION['id_usuario'])){
+        header("location: login.php");
+        exit;
+    }
+
+    require "./class-php/user.class.php";
+    $user = new User("museu", "localhost", "root", "");
 
 ?>
 
