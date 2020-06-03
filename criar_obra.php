e5d0677c47e55b1531eba82ed64c4ea241622e84
Jorzias Veras <josias_veras@hotmail.com>
2020-06-03 14:20:21 -0300

    Update criar_obra.php

--- criar_obra.php
+++ criar_obra.php
@@ -1,3 +1,11 @@
+<?php    
+    session_start();
+    if(!isset($_SESSION['id_usuario'])){
+        header("location: login.php");
+        exit;
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
