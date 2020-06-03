02dcfdec5cb9520dc64924a8e13dcf9e38b4b0f3
Jorzias Veras <josias_veras@hotmail.com>
2020-06-03 19:09:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -39,10 +39,19 @@
                 <!-- Início nav -->
                  <nav>
                     <ul>
-                        <li><a href="./index.php">Home</a></li>
-                        <li><a href="./visualizar_obras.php">Exposições</a></li>
-                        <li><a href="./criar_obra.php" target="_blank">Criar obra</a></li>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="visualizar_obras.php">Exposições</a></li>
+                        <li><a href="criar_obra.php" target="_blank">Criar obra</a></li>
                     </ul>
+
+                    <div class="dropdown">
+                        <button class="dropbtn">Olá, Jorzias</button>
+                        <div class="dropdown-content">
+                            <a href="./">Perfil</a>
+                            <a href="#" onclick="">Sair</a>
+                        </div>
+                    </div>
+
                 </nav>
                 <!-- Fim nav -->
 
