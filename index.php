7d8cd92c77e4285d2419873435103a0ee1178f5a
Jorzias Veras <josias_veras@hotmail.com>
2020-06-03 14:20:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -34,16 +34,15 @@
             <header>
                 <div id="logo">
                     <h1><a href="">MuSenac</a></h1>
-                </div>
+                </div> 
 
                 <!-- Início nav -->
-                <nav>
+                 <nav>
                     <ul>
                         <li><a href="./index.php">Home</a></li>
                         <li><a href="./visualizar_obras.php">Exposições</a></li>
                         <li><a href="./criar_obra.php" target="_blank">Criar obra</a></li>
                     </ul>
-
                 </nav>
                 <!-- Fim nav -->
 
