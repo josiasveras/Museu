6460c4e6de97a64adc785f1e1281d9ef235221d9
Jorzias Veras <josias_veras@hotmail.com>
2020-06-07 20:05:49 -0300

    Update perfil_usuario.php

--- perfil_usuario.php
+++ perfil_usuario.php
@@ -4,6 +4,8 @@
     if(!isset($_SESSION['id_usuario'])){
         header("location: login.php");
         exit;
+    }else{
+        $id = $_SESSION['id_usuario'];
     }
 
     require "./class-php/user.class.php";
@@ -19,6 +21,32 @@
     </head>
 
     <body>
+        
+        <?php 
+
+            $dadosUsuario = $user->selectUserById($id);
+
+            if (isset($_POST['nome'])) {
+                //Recebendo o que o usuário digita nos inputs
+                $nome = addslashes($_POST['nome']);
+                $email = addslashes($_POST['email']);
+                //$senha = addslashes($_POST['senha']);
+                $idade = addslashes($_POST['idade']);
+
+                //Verificando se os campos não estão vazios
+                if (!empty($nome) && !empty($email) && !empty($idade)) {
+                      
+                    $user->updateUser($id, $nome, $email, $idade);
+
+                }else{
+
+                    echo '<div class="alert alert-danger"" role="alert"> Preencha todos os campos! </div>';
+
+                }
+
+            }
+
+        ?>
 
         <!-- Início container -->
         <div id="container">
@@ -63,24 +91,19 @@
                         }*/
                     ?>
 
-                    <form>
+                    <form method="POST">
                       <div class="form-group">
                         <label for="nome">Nome</label>
-                        <input type="text" class="form-control" id="nome" name="nome">
+                        <input type="text" class="form-control" id="nome" name="nome" value="<?php echo $dadosUsuario['nome_usuario']; ?>">
                       </div>
                       <div class="form-group">
                         <label for="email">Endereço de email</label>
-                        <input type="email" class="form-control" id="email" name="email">
+                        <input type="email" class="form-control" id="email" name="email" value="<?php echo $dadosUsuario['email']; ?>">
                       </div>
                       <div class="form-group">
-                        <label for="dtNasc">Data de nascimento</label>
-                        <input type="text" class="form-control" id="dtNasc" name="dtNasc">
-                      </div>
-                      <div class="form-group">
-                        <label for="genero">Gênero</label>
-                        <input type="text" class="form-control" id="genero" name="genero">
-                      </div>
-                      
+                        <label for="dtNasc">Idade</label>
+                        <input type="number" maxlength="3" class="form-control" id="idade" name="idade" value="<?php echo $dadosUsuario['idade']; ?>">
+                      </div> 
                       <button type="submit" class="btn btn-primary">Salvar</button>
                     </form>
                     
