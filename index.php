75eaac418a26bf7f873b1dc53231eeaa2766d2c9
Jorzias Veras <josias_veras@hotmail.com>
2020-06-08 15:43:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,7 @@
         <title>Museu SENAC</title>
         <meta charset="UTF-8">
 
-        <!-- Normalize CSS -->
-        <link rel="stylesheet" type="text/css" href="css/normalize.css">
-        
-        <!-- Estilo customizado -->
-        <link rel="stylesheet" type="text/css" href="css/estilo.css">
-
-        <!-- html5shiv para fazer com que os navegadores que não reconheçam as novas tags HTML5, passem a reconhecê-las -->
-        <!--[if lt IE 9]>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
-        <![endif]-->
+        <?php require "./head.php"; ?>
 
     </head>
 
@@ -32,6 +23,7 @@
 
             <!-- Início header -->
             <header>
+
                 <div id="logo">
                     <h1><a href="">MuSenac</a></h1>
                 </div> 
@@ -43,7 +35,12 @@
                         <li><a href="visualizar_obras.php">Exposições</a></li>
                         <li><a href="criar_obra.php" target="_blank">Criar obra</a></li>
                     </ul>
+                    
+                    
+                </nav>
+                <!-- Fim nav -->
 
+                <!-- Início botão logado/não logado -->
                     <?php
 
                         session_start();  
@@ -73,8 +70,7 @@
 
                         } 
                     ?>
-                </nav>
-                <!-- Fim nav -->
+                    <!-- Fim botão logado/não logado -->
 
             </header>
             <!-- Fim header -->
