e16e806749a76756f19f49a3275f25ed5ec101b7
Jorzias Veras <josias_veras@hotmail.com>
2020-06-08 15:44:13 -0300

    Update cadastro_usuario.php

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -14,6 +14,112 @@
 
     <body>
 
+                    <!-- Início botão logado/não logado -->
+                    <?php
+
+                        session_start();  
+                        
+                        if(!isset($_SESSION['id_usuario'])){
+                                                   
+                    ?>
+                                <div class="dropdown">
+                                    <button class="dropbtn">Não Logado</button>
+                                    <div class="dropdown-content">
+                                        <a href="login.php">Login</a>
+                                        <a href="cadastro_usuario.php">Inscrever-se</a>
+                                    </div>
+                                </div>
+                    <?php
+                        }else{
+
+                            /*session_start();*/
+
+                            echo'<div class="dropdown">
+                                    <button class="dropbtn">Logado</button>
+                                    <div class="dropdown-content">
+                                        <a href="perfil_usuario.php">Perfil</a>
+                                        <a href="logout.php">Sair</a>
+                                    </div>
+                                </div>';
+
+                        } 
+                    ?>
+                    <!-- Fim botão logado/não logado -->
+
+        <!-- Início container -->
+        <div id="container">
+
+            <!-- Início header -->
+            <header class="headerPos">
+                <div id="logo">
+                    <h1><a href="">MuSenac</a></h1>
+                </div>
+
+                <!-- Início nav -->
+                <nav>
+                    <ul>
+                        <li><a href="./index.php">Home</a></li>
+                        <li><a href="./visualizar_obras.php">Exposições</a></li>
+                        <li><a href="./criar_obra.php" target="_blank">Criar obra</a></li>
+                    </ul>
+                </nav>
+                <!-- Fim nav -->
+
+            </header>
+            <!-- Fim header -->
+
+            <!-- Início principal -->
+            <div id="principal">
+                <br><br><br><br><br><br>
+                <!-- Início form-wrapper -->
+                <!-- <div class="form-wrapper">
+                    <h2 style="text-align:center;">Criação de perfil público</h2>
+                    <form method="POST">
+                      <div class="form-group">
+                        <label for="nome">Nome</label>
+                        <input type="text" class="form-control" id="nome" name="nome">
+                      </div>
+                      <div class="form-group">
+                        <label for="email">Endereço de email</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Ex: caracter@caracter.dominio">
+                      </div>
+                      <div class="form-group">
+                        <label for="senha">Senha</label>
+                        <input type="password" class="form-control" id="senha" name="senha" placeholder="6 caracteres">
+                      </div>
+                      <div class="form-group">
+                        <label for="dtNasc">Idade</label>
+                        <input type="number" maxlength="3" class="form-control" id="idade" name="idade">
+                      </div> 
+                      <br><br>
+                      <button type="submit" class="btn btn-primary">Enviar</button>
+                    </form>
+                </div> -->
+                <!-- Fim form-wrapper -->
+
+        <!-- Início containerForm -->
+        <div class="containerForm">
+          <h2 style="text-align:center;">Criação de perfil público</h2>  
+          <form method="POST">
+
+            <label for="nome">Nome</label>
+            <input type="text" id="nome" name="nome">
+
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Ex: caracter@caracter.dominio">
+
+            <label for="senha">Senha</label>
+            <input type="password" id="senha" name="senha" placeholder="6 caracteres">
+
+            <label for="idade">Idade</label>
+            <input type="number" maxlength="3" id="idade" name="idade">
+
+            <input type="submit" value="Enviar">
+
+          </form>
+        </div>
+        <!-- Fim containerForm -->
+
         <?php 
 
             if (isset($_POST['nome'])) {
@@ -44,57 +150,6 @@
             }
 
         ?>
-
-        <!-- Início container -->
-        <div id="container">
-
-            <!-- Início header -->
-            <header>
-                <div id="logo">
-                    <h1><a href="">MuSenac</a></h1>
-                </div>
-
-                <!-- Início nav -->
-                <nav>
-                    <ul>
-                        <li><a href="./index.php">Home</a></li>
-                        <li><a href="./visualizar_obras.php">Exposições</a></li>
-                        <li><a href="./criar_obra.php" target="_blank">Criar obra</a></li>
-                    </ul>
-                </nav>
-                <!-- Fim nav -->
-
-            </header>
-            <!-- Fim header -->
-
-            <!-- Início principal -->
-            <div id="principal">
-                <br><br><br><br><br><br>
-                <!-- Início form-wrapper -->
-                <div class="form-wrapper">
-                    <h2 style="text-align:center;">Criação de perfil público</h2>
-                    <form method="POST">
-                      <div class="form-group">
-                        <label for="nome">Nome</label>
-                        <input type="text" class="form-control" id="nome" name="nome">
-                      </div>
-                      <div class="form-group">
-                        <label for="email">Endereço de email</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Ex: caracter@caracter.dominio">
-                      </div>
-                      <div class="form-group">
-                        <label for="senha">Senha</label>
-                        <input type="password" class="form-control" id="senha" name="senha" placeholder="6 caracteres">
-                      </div>
-                      <div class="form-group">
-                        <label for="dtNasc">Idade</label>
-                        <input type="number" maxlength="3" class="form-control" id="idade" name="idade">
-                      </div> 
-                      <br><br>
-                      <button type="submit" class="btn btn-primary">Enviar</button>
-                    </form>
-                </div>
-                <!-- Fim form-wrapper -->
 
                 <footer>
                     <p>
